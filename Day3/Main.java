6a152211e30799c6e2c80faed08189a86b997451
cedney97 <c.edney@wustl.edu>
2022-12-21 15:31:10 -0600

    Got rid of warning

--- Day3/Main.java
+++ Day3/Main.java
@@ -2,7 +2,6 @@ package Day3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
